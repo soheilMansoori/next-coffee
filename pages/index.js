5bb6e845e4e8bdc3da41014f6d9af3050bed1885
soheilMansoori <ahadmansoori369@gmail.com>
2024-01-01 00:08:37 +0330

    import Reservation.jsx in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import About from "@/components/templates/index/About/About";
 import Menu from "@/components/templates/index/Menu/Menu";
 import Offer from "@/components/templates/index/Offer/Offer";
+import Reservation from "@/components/templates/index/Reservation/Reservation";
 import Service from "@/components/templates/index/Service/Service";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Service />
       <Offer />
       <Menu />
+      <Reservation />
     </>
   )
 }
