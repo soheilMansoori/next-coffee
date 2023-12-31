72c69f9742a17d1c011d320042e69feadaf65a79
soheilMansoori <ahadmansoori369@gmail.com>
2023-12-31 22:35:36 +0330

    fix icons in Service.jsx

--- components/templates/index/Service/Service.jsx
+++ components/templates/index/Service/Service.jsx
@@ -1,3 +1,5 @@
+import { faAward, faCoffee, faTable, faTruck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 export default function Service() {
@@ -5,7 +7,7 @@ export default function Service() {
         <div className="container-fluid pt-5">
             <div className="container">
                 <div className="section-title">
-                    <h4 className="text-primary text-uppercase" style={{letterSpacing: "5px"}}>Our Services</h4>
+                    <h4 className="text-primary text-uppercase" style={{ letterSpacing: "5px" }}>Our Services</h4>
                     <h1 className="display-4">Fresh & Organic Beans</h1>
                 </div>
                 <div className="row">
@@ -15,7 +17,10 @@ export default function Service() {
                                 <img className="img-fluid mb-3 mb-sm-0" src="images/service-1.jpg" alt="" />
                             </div>
                             <div className="col-sm-7">
-                                <h4><i className="fa fa-truck service-icon"></i>Fastest Door Delivery</h4>
+                                <h4>
+                                    <FontAwesomeIcon icon={faTruck} className='service-icon' />
+                                    Fastest Door Delivery
+                                </h4>
                                 <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
                                     invidunt, dolore tempor diam ipsum takima erat tempor</p>
                             </div>
@@ -27,7 +32,10 @@ export default function Service() {
                                 <img className="img-fluid mb-3 mb-sm-0" src="images/service-2.jpg" alt="" />
                             </div>
                             <div className="col-sm-7">
-                                <h4><i className="fa fa-coffee service-icon"></i>Fresh Coffee Beans</h4>
+                                <h4>
+                                    <FontAwesomeIcon icon={faCoffee} className='service-icon' />
+                                    Fresh Coffee Beans
+                                </h4>
                                 <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
                                     invidunt, dolore tempor diam ipsum takima erat tempor</p>
                             </div>
@@ -39,7 +47,10 @@ export default function Service() {
                                 <img className="img-fluid mb-3 mb-sm-0" src="images/service-3.jpg" alt="" />
                             </div>
                             <div className="col-sm-7">
-                                <h4><i className="fa fa-award service-icon"></i>Best Quality Coffee</h4>
+                                <h4>
+                                    <FontAwesomeIcon icon={faAward} className='service-icon' />
+                                    Best Quality Coffee
+                                </h4>
                                 <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
                                     invidunt, dolore tempor diam ipsum takima erat tempor</p>
                             </div>
@@ -51,7 +62,10 @@ export default function Service() {
                                 <img className="img-fluid mb-3 mb-sm-0" src="/images/service-4.jpg" alt="" />
                             </div>
                             <div className="col-sm-7">
-                                <h4><i className="fa fa-table service-icon"></i>Online Table Booking</h4>
+                                <h4>
+                                    <FontAwesomeIcon icon={faTable} className='service-icon' />
+                                    Online Table Booking
+                                </h4>
                                 <p className="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
                                     invidunt, dolore tempor diam ipsum takima erat tempor</p>
                             </div>
