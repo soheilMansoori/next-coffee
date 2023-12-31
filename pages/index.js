42d80820f59d0da2819121e3d05057cbb5d9f5b8
soheilMansoori <ahadmansoori369@gmail.com>
2023-12-31 22:36:45 +0330

    import Service.jsx in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import About from "@/components/templates/index/About/About";
+import Service from "@/components/templates/index/Service/Service";
 
 export default function Home() {
   return (
     <>
       <About />
+      <Service />
     </>
   )
 }
