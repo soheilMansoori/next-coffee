5bc322b0a2fefd9b3abe1b45e1a899efdf6ac269
soheilMansoori <ahadmansoori369@gmail.com>
2024-01-01 22:46:05 +0330

    import Slider.jsx in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Menu from "@/components/templates/index/Menu/Menu";
 import Offer from "@/components/templates/index/Offer/Offer";
 import Reservation from "@/components/templates/index/Reservation/Reservation";
 import Service from "@/components/templates/index/Service/Service";
+import Slider from "@/components/templates/index/Slider/Slider";
 import Testimonial from "@/components/templates/index/Testimonial/Testimonial";
 
 export default function Home() {
   return (
     <>
+      <Slider />
       <About />
       <Service />
       <Offer />
