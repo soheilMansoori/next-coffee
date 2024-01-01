b91160ba31c0d0d754a95bc0c1554eafd45a162d
soheilMansoori <ahadmansoori369@gmail.com>
2024-01-01 22:41:42 +0330

    remove Slider.jsx in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import BackToTop from '@/components/modules/BackToTop/BackToTop'
 import Footer from '@/components/modules/Footer/Footer'
 import Navbar from '@/components/modules/Navbar/Navbar'
-import Slider from '@/components/modules/Slider/Slider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
-      <Slider />
       <Component {...pageProps} />
       <BackToTop />
       <Footer />
