5006fc4fbff47628fadf899d3a40d8971f02c438
soheilMansoori <ahadmansoori369@gmail.com>
2024-07-09 22:13:08 +0330

    fix Header headerTitle to dynamic with rout

--- components/modules/Header/Header.jsx
+++ components/modules/Header/Header.jsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
-export default function Header({ title }) {
+export default function Header() {
+    const { pathname } = useRouter();
+    const [_, routName] = pathname.split("/");
+    const firstLetterUppercaseHandler = (text = "") => text.charAt(0).toUpperCase() + text.slice(1);
+    const headerTitle = firstLetterUppercaseHandler(routName);
+
     return (
         <div className="container-fluid page-header mb-5 position-relative overlay-bottom">
             <div className="d-flex flex-column align-items-center justify-content-center pt-0 pt-lg-5" style={{ minHeight: "400px" }}>
                 <h1 className="display-4 mb-3 mt-0 mt-lg-5 text-white text-uppercase">{title}</h1>
                 <div className="d-inline-flex mb-lg-5">
-                    <p className="m-0 text-white"><a className="text-white" href="">Home</a></p>
+                    <p className="m-0 text-white">
+                        <Link href="/" className="text-white">Home</Link>
+                    </p>
                     <p className="m-0 text-white px-2">/</p>
-                    <p className="m-0 text-white">{title}</p>
+                    <p className="m-0 text-white">{headerTitle}</p>
                 </div>
             </div>
         </div>
