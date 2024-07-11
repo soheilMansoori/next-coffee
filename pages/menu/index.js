3787903be998e5a76bf09870b850fa010b2911c3
soheilMansoori <ahadmansoori369@gmail.com>
2024-07-11 19:36:01 +0330

    create MenuPage

---
+++ pages/menu/index.js
@@ -0,0 +1,11 @@
+import Header from "@/components/modules/Header/Header";
+import Menu from "@/components/modules/Menu/Menu";
+
+export default function MenuPage() {
+    return (
+        <>
+            <Header />
+            <Menu />
+        </>
+    )
+}
