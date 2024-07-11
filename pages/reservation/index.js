a878d6122eec56fcbd2a49f51fb08d620edd623e
soheilMansoori <ahadmansoori369@gmail.com>
2024-07-11 19:38:56 +0330

    create and fix ReservationPage

---
+++ pages/reservation/index.js
@@ -0,0 +1,12 @@
+import Header from '@/components/modules/Header/Header'
+import Reservation from '@/components/modules/Reservation/Reservation'
+import React from 'react'
+
+export default function ReservationPage() {
+    return (
+        <>
+            <Header />
+            <Reservation />
+        </>
+    )
+}
