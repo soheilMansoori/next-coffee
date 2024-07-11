35422bb4ef20d558c6dbe5be291891ed0c501231
soheilMansoori <ahadmansoori369@gmail.com>
2024-07-11 19:41:06 +0330

    create and fix TestimonialPage

---
+++ pages/testimonial/index.js
@@ -0,0 +1,11 @@
+import Header from '@/components/modules/Header/Header'
+import Testimonial from '@/components/modules/Testimonial/Testimonial'
+
+export default function TestimonialPage() {
+    return (
+        <>
+            <Header />
+            <Testimonial />
+        </>
+    )
+}
