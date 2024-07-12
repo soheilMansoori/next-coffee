d03caac5e0659e16931efed90c2716ff78df8c39
soheilMansoori <ahadmansoori369@gmail.com>
2024-07-12 14:22:30 +0330

    fix Offer form

--- components/templates/Home/Offer/Offer.jsx
+++ components/templates/Home/Offer/Offer.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Offer() {
+    const [emailValue, setEmailValue] = useState("");
+    // send email to server 
+    const sendEmailToSever = (event) => {
+        event.preventDefault();
+        const emailRegex = new RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/i);
+        if (emailRegex.test(emailValue)) {
+            fetch("http://localhost:4000/newsLetters", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email: emailValue })
+            }).then(res => {
+                if (res.ok) {
+                    setEmailValue("");
+                    alert("you join successfully");
+                } else {
+                    alert("cant add your email server error");
+                }
+            }).catch(error => alert(error.message));
+
+        } else {
+            alert("email is not valid")
+        }
+    }
     return (
         <div className="offer container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom">
             <div className="container py-5">
                 <h1 className="display-3 text-primary mt-3">50% OFF</h1>
                 <h1 className="text-white mb-3">Sunday Special Offer</h1>
                 <h4 className="text-white font-weight-normal mb-4 pb-3">Only for Sunday from 1st Jan to 30th Jan 2045</h4>
-                <form className="form-inline justify-content-center mb-4">
+                <form onSubmit={sendEmailToSever} className="form-inline justify-content-center mb-4">
                     <div className="input-group">
-                        <input type="text" className="form-control p-4" placeholder="Your Email" style={{height: "60px"}} />
-                            <div className="input-group-append">
-                                <button className="btn btn-primary font-weight-bold px-4" type="submit">Sign Up</button>
-                            </div>
+                        <input
+                            type="text"
+                            className="form-control p-4"
+                            placeholder="Your Email"
+                            style={{ height: "60px" }}
+                            value={emailValue}
+                            onChange={(event) => setEmailValue(event.target.value)}
+                        />
+                        <div className="input-group-append">
+                            <button className="btn btn-primary font-weight-bold px-4" type="submit">Join</button>
+                        </div>
                     </div>
                 </form>
             </div>
